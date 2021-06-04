61a731c61d92b4f347eb5b9dfc13af9dbaa69544
Doug Gale <doug16k@gmail.com>
2021-06-04 14:18:05 -0400

    Handle assert nesting

--- assert.cc
+++ assert.cc
@@ -2,8 +2,12 @@
 #include "debug.h"
 #include "arch/halt.h"
 
+static int assert_nest;
+
 void __assert_failed(const char *expr, const char *file, int line)
 {
-    printdbg("%s(%d): Assertion failed: %s\n", file, line, expr);
+    int nesting = __atomic_fetch_add(&assert_nest, 1, __ATOMIC_RELAXED);
+    if (!nesting)
+        printdbg("%s(%d): Assertion failed: %s\n", file, line, expr);
     arch_halt();
 }
