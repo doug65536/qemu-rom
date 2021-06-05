bd8bbf0be92dbf246255f91e69c5694ed2ac6c9c
Doug Gale <doug16k@gmail.com>
2021-06-05 11:02:49 -0400

    Add a missing arch_halt()

--- arch/x86_shared/portio.cc
+++ arch/x86_shared/portio.cc
@@ -1,11 +1,13 @@
 #include "portio.h"
+#include "arch/halt.h"
+#include "debug.h"
 
 void arch_debug_char(uint8_t ch)
 {
     outb(0xe9, ch);
 }
 
-extern "C" void arch_halt()
+void arch_halt()
 {
     while (true)
         __asm__("hlt");
